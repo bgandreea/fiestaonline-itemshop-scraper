03f994142f4a942a79eafaaca4bc2628d3a1295c
Andreea Baboi <andreea.georgiana.baboi@gmail.com>
2024-02-03 14:01:58 +0100

    Update run_bot.py
    Updated run_bot with environment variables stored in separate file for easier control

--- run_bot.py
+++ run_bot.py
@@ -2,6 +2,7 @@ import discord
 from discord import app_commands
 
 from utilities.helper import scan_for_items, client
+from utilitie import environment
 
 # Initialize Discord bot
 tree = app_commands.CommandTree(client)
@@ -9,17 +10,17 @@ tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=your_discord_server_id))
+    await tree.sync(guild=discord.Object(id=environment.DISCORD_SERVER_ID))
     print("Ready!")
 
 
 @tree.command(name="scan_shop",
               description="Function that scans the Fiesta Online shop for Permanent items",
-              guild=discord.Object(id=your_discord_server_id))
+              guild=discord.Object(id=environment.DISCORD_SERVER_ID))
 async def scan_shop(interaction):
     await interaction.response.send_message("@everyone Today's List of Permanent Items:")
     channel_id = interaction.channel_id
-    await scan_for_items(channel_id)
+    await scan_for_items(environment.CHANNEL_ID)
 
 
-client.run("your_discord_bot_secret_key")
+client.run(f"{environment.DISCORD_BOT_SECRET_KEY}")
